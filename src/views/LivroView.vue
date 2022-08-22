299b8481d71a98b8f4e0af8ae8942767a292c195
GCFilmes <cardozogumsc@gmail.com>
2022-08-22 11:53:29 -0300

    commit

--- src/views/LivroView.vue
+++ src/views/LivroView.vue
@@ -26,16 +26,16 @@
                     <v-card>
                         <h3 class="text-uppercase text-center text-decoration-underline pt-3 pb-3" style="color: #114B5F;"> Publique uma resenha:</h3>
                         <div class="ma-5">
-                            <v-text-field label="Título" outlined color="#114B5F"></v-text-field>
-                            <v-textarea color="#114B5F" outlined name="input-7-4" label="Resenha"></v-textarea>
+                            <v-text-field label="Título" outlined color="#114B5F" v-model="novoTitulo"></v-text-field>
+                            <v-textarea color="#114B5F" outlined name="input-7-4" label="Resenha" v-model="novaResenha"></v-textarea>
                             <v-rating class="" style="" background-color="#F1C40F" color="#F1C40F" empty-icon="mdi-star-outline" full-icon="mdi-star" half-icon="mdi-star-half-full" 
-                            hover length="5" size="25" value="1.5"></v-rating>
+                            hover length="5" size="25" v-model="rating" ref="rating"></v-rating>
                         </div>
                          
                         <v-card-actions>
                         <v-spacer></v-spacer>
                         <v-btn color="#FF0000" text @click="dialog = false"> Cancelar </v-btn>
-                        <v-btn style="color: #114B5F;" color="#CAF1FF" text @click="dialog = false"> Enviar </v-btn>
+                        <v-btn style="color: #114B5F;" color="#CAF1FF" text @click="adicionarResenha"> Enviar </v-btn>
                         </v-card-actions>
                     </v-card>
                 </v-dialog>
@@ -78,53 +78,22 @@
             </div>
         </v-col>
         <v-col  cols="8">
-            <div style="">
-                <h3 class="text-center text-uppercase text-decoration-underline mb-0" style="color: #114B5F;">Resenhas da comunidade</h3>
-                <div class="d-flex justify-center flex-column" >
-                    <div class="d-flex justify-center">
-                        <v-card class="rounded-lg ma-5" elevation="4" color="#114B5F" style="width: 35rem;">
+            <div class="justify-center" style="">
+                <h3 class="text-center text-uppercase text-decoration-undersline mb-0" style="color: #114B5F;">Resenhas da comunidade</h3>
+                <div class="d-flex justify-center" >
+                    <div class="d-flex justify-center flex-column ">
+                        <v-card class="rounded-lg ma-5" elevation="4" color="#114B5F" style="width: 35rem;" 
+                            v-for="(comentario, index) in resenha" :key="index">
                             <v-col class=" d-flex align-center">
                                 <v-avatar class="mr-5" color="#FFF" size="50">GA</v-avatar>
-                                <h4 class="font-weight-light" style="color: #FFF;">Username</h4>
+                                <h4 class="font-weight-light" style="color: #FFF;">{{ comentario.titulo }}</h4>
                                 <v-spacer></v-spacer>
                                 <v-rating class="" style="" background-color="#F1C40F" color="#F1C40F" empty-icon="mdi-star-outline" full-icon="mdi-star" half-icon="mdi-star-half-full" 
-                                hover length="5" readonly size="25" value="4.5"></v-rating> 
+                                hover length="5" readonly size="25" :value="comentario.estrela"></v-rating> 
                             </v-col>
                             <v-col class="d-flex justify-center">
                                 <v-card class="rounded-lg" style="width: 30rem;">
-                                    <p class="text-left ma-3"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id neque vel justo tincidunt varius id et est. Cras sit amet turpis et risus ultricies vestibulum et eu dui. Sed pretium leo in fermentum consectetur. Donec id tincidunt purus.  </p>
-                                </v-card>                        
-                            </v-col>
-                        </v-card>                        
-                    </div>
-                    <div class="d-flex justify-center">
-                        <v-card class="rounded-lg ma-5" elevation="4" color="#114B5F" style="width: 35rem;">
-                            <v-col class=" d-flex align-center">
-                                <v-avatar class="mr-5" color="#FFF" size="50">JV</v-avatar>
-                                <h4 class="font-weight-light" style="color: #FFF;">Username</h4>
-                                <v-spacer></v-spacer>
-                                <v-rating class="" style="" background-color="#F1C40F" color="#F1C40F" empty-icon="mdi-star-outline" full-icon="mdi-star" half-icon="mdi-star-half-full" 
-                                hover length="5" readonly size="25" value="4.5"></v-rating> 
-                            </v-col>
-                            <v-col class="d-flex justify-center">
-                                <v-card class="rounded-lg" style="width: 30rem;">
-                                    <p class="text-left ma-3"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id neque vel justo tincidunt varius id et est. Cras sit amet turpis et risus ultricies vestibulum et eu dui. Sed pretium leo in fermentum consectetur. Donec id tincidunt purus.  </p>
-                                </v-card>                        
-                            </v-col>
-                        </v-card>                        
-                    </div>
-                    <div class="d-flex justify-center">
-                        <v-card class="rounded-lg ma-5" elevation="4" color="#114B5F" style="width: 35rem;">
-                            <v-col class=" d-flex align-center">
-                                <v-avatar class="mr-5" color="#FFF" size="50">YM</v-avatar>
-                                <h4 class="font-weight-light" style="color: #FFF;">Username</h4>
-                                <v-spacer></v-spacer>
-                                <v-rating class="" style="" background-color="#F1C40F" color="#F1C40F" empty-icon="mdi-star-outline" full-icon="mdi-star" half-icon="mdi-star-half-full" 
-                                hover length="5" readonly size="25" value="4.5"></v-rating> 
-                            </v-col>
-                            <v-col class="d-flex justify-center">
-                                <v-card class="rounded-lg" style="width: 30rem;">
-                                    <p class="text-left ma-3"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id neque vel justo tincidunt varius id et est. Cras sit amet turpis et risus ultricies vestibulum et eu dui. Sed pretium leo in fermentum consectetur. Donec id tincidunt purus.  </p>
+                                    <p class="text-left ma-3"> {{ comentario. descricao }}  </p>
                                 </v-card>                        
                             </v-col>
                         </v-card>                        
@@ -141,7 +110,23 @@ export default {
     data: () => ({ 
       power: 75,
       dialog: false,
+      novoTitulo: "",
+      novaResenha: "",
+      resenha: [
+        { descricao: "livro muito bomlivro muito bomlivro muito bom", titulo: "gostei do livro", estrela: 3},
+       
+      ],
     }),
+    methods: {
+        adicionarResenha() {
+            this.resenha.push({
+                descricao: this.novaResenha,
+                titulo: this.novoTitulo,
+                estrela: this.rating
+            })
+            this.dialog = false
+        }
+    }
 }
 </script>
 
