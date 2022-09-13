f1dace32985deb834ec7ad446def86dea6869f15
gabriel04alves <gabriel52.alves@gmail.com>
2022-09-12 21:13:10 -0300

    Commit - pequenas alterações

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,8 +8,8 @@
             <h2 class="text-decoration-underline mt-10 font-weight-light" style="color: #114B5F">BEM VINDO</h2> 
             <p class="font-weight-bold mt-2" style="color: #114B5F">Faça seu login</p>
             <v-form class="">
-              <v-text-field color="#114B5F" label="Email" v-model="user.email" style=""></v-text-field>
-              <v-text-field color="#114B5F" class="mt-0" label="Senha" v-model="user.password" :type="show ? 'text' : 'password'" :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'" @click:append="show = !show" ></v-text-field>  
+              <v-text-field color="#114B5F" label="Email" v-model="user.email" style="" required outlined></v-text-field>
+              <v-text-field color="#114B5F" class="mt-0" label="Senha" v-model="user.password" :type="show ? 'text' : 'password'" :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'" @click:append="show = !show" required outlined></v-text-field>  
               <v-row class="d-flex justify-center mt-5">
                 <v-btn class="rounded-lg" color="#FFF" style="color: #114B5F;" @click="login">Entrar</v-btn>
               </v-row>
