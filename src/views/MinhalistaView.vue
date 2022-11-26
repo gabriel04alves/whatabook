073c8d98ee05429883b3dc6916f862d100c66f50
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-11-26 12:26:39 -0300

    minha lista view post/delete

--- src/views/MinhalistaView.vue
+++ src/views/MinhalistaView.vue
@@ -3,16 +3,51 @@
     <v-row data-aos="fade-right" class="ma-10" >
       <v-icon x-large color="#114B5F">mdi-book-heart</v-icon>
       <h1 class="ml-5 text-uppercase" style="color:#114B5F;">Minha lista</h1>
+      <v-row align="center" class="ml-6 flex-start">
+        <v-btn tile color="#caf1ff" @click="dialog=true"><v-icon left>mdi-plus</v-icon>Nova lista</v-btn>
+      </v-row>
     </v-row>
+    <v-dialog v-model="dialog" persistent max-width="500">
+      <v-card>
+        <h3 class="text-uppercase text-center text-decoration-underline pt-3 pb-3" style="color: #114b5f">
+          Crie uma lista:
+        </h3>
+        <div class="ma-5">
+          <v-text-field label="Nome da lista" outlined color="#114B5F" v-model="listanome" @keyup.enter="criarLista"></v-text-field>
+        </div>
+        <v-card-actions>
+          <div v-if="erro" style="color: #FF0000">Ocorreu um erro</div>
+          <v-spacer></v-spacer>
+          <v-btn color="#FF0000" text @click="dialog = false">Cancelar</v-btn>
+          <v-btn style="color: #114b5f"  color="#CAF1FF" text @click="criarLista">Criar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-row class="pt-8">
-      <v-row data-aos="flip-down" v-for="item,index in categorias" :key="index" class="pt-0 pb-3">
+      <v-row data-aos="flip-down" v-for="lista,index in listas" :key="index" class="pt-0 pb-3 d-flex flex-column">
+        <div class="d-flex align-center">
+          <h3 class="pt-5 text-uppercase font-weight-light" style="padding-left: 5%;">{{ lista.titulo_lista }}</h3>
+          <v-btn outlined tile class="ml-3 mt-5" color="error" @click="escolherLista(lista)"><v-icon left>mdi-trash-can</v-icon>Excluir</v-btn>
+        <v-dialog v-model="dialog1" persistent max-width="500">
+          <v-card>
+            <h3 class="text-uppercase text-center text-decoration-underline pt-3 pb-3" style="color: #114b5f">
+              Excluir lista?
+            </h3>
+            <v-card-title>Excluir "{{delLista.titulo_lista}}"?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn style="color: #114b5f"  color="#CAF1FF" text @click="dialog1 = false">Cancelar</v-btn>
+              <v-btn color="#FF0000" text @click="excluirLista(delLista.id)">Excluir</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        </div>
         <v-sheet width="calc(135rem - 200px)" color="#FFF" style="color: #114B5F">
-          <h3 class="pt-5 text-uppercase font-weight-light" style="padding-left: 5%;">{{ item.slogan }}</h3>
           <v-slide-group  class="pt-2 pb-4 pr-4 pl-4" show-arrows>
-              <v-slide-item v-for="imagem in livro" :key="imagem" v-show="imagem.categoria == livro.nomeLivro">
+              <v-slide-item v-for="livro,index in lista.livros_lista" :key="index">
                 <div class="ma-4" style="height: 100%; width: 8.1rem">
                 <v-row class="fill-height" justify="center">
-                  <img @click="irParaLivro(imagem)" id="img" class="ma-2" :src="imagem.imagemLivro" height="200" width="140" />
+                  <img @click="$router.push({name: 'livro', params: {id: livro.id}})" style="cursor: pointer" id="img" class="ma-2" :src="livro.capa_livro" height="200" width="140" />
                 </v-row>
               </div>
               </v-slide-item>
@@ -24,19 +59,41 @@
 </template>
 
 <script>
+import axios from "axios"
   export default {
     data: () => ({
-      items: [
-      
-    ],
-      categorias: [
-        {title: "Meus favoritos", slogan: "Meus favoritos", desc: "genero textual que tem como caracteristica..."},
-        {title: "Para comprar mais tarde", slogan: "Para comprar mais tarde", desc: "genero textual que tem como caracteristica..."},
-        {title: "Gostei", slogan: "Gostei", desc: "genero textual que tem como caracteristica..."},
-        {title: "Para compartilhar", slogan: "Para compartilhar", desc: "genero textual que tem como caracteristica..."},
-      ],
+      dialog: false,
+      dialog1: false,
+      erro: false,
+      listanome: '',
+      listas: [],
       livro: [],
+      delLista: {},
     }),
+    mounted(){
+      this.getListas()
+    },
+    methods:{
+      async criarLista(){
+        await axios.post('/api/listafav/', {titulo_lista: this.listanome})
+        this.getListas()
+        this.dialog = false
+      },
+      async getListas(){
+        const {data} = await axios.get('/api/listafav/')
+        this.listas = data
+        console.log(this.listas)
+      },
+      async excluirLista(id){
+        await axios.delete(`/api/listafav/${id}/`)
+        this.dialog1 = false
+        this.getListas()
+      },
+      escolherLista(lista){
+        this.delLista = lista
+        this.dialog1 = true
+      }
+    }
   }
 </script>
 
