54ff627cb84c841683381256b7b8fa3a4b4a1be2
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-11-26 18:46:35 -0300

    Pagina de categorias

--- src/views/CategoriasView.vue
+++ src/views/CategoriasView.vue
@@ -5,25 +5,19 @@
     </div>
 
     <v-carousel class="" style="width: 100%;" v-model="model" cycle hide-delimiter-background :show-arrows="false">
-      <v-carousel-item v-for="(color, i) in colors" :key="i" height="110%" width="100%" >
-        <v-sheet :color="color" height="110%" width="100%" tile >
-          <v-row class="fill-height" align="center" justify="center" >
-            <div class="text-h2">
-              Whatabook {{ i + 1 }}
-            </div>
-          </v-row>
-        </v-sheet>
+      <v-carousel-item v-for="(imagem, i) in imagens" :key="i" height="110%" width="100%" >
+        <div class="imagem" :style="'background-image: url(' + imagem +'); height: 110%; width: 100%'"></div>
       </v-carousel-item>
     </v-carousel>
     <v-row class="pt-8">
-      <v-row data-aos="flip-down" v-for="item,index in categorias" :key="index" class="pt-0 pb-3">
+      <v-row data-aos="flip-down" v-for="categoria,index in categorias" :key="index" class="pt-0 pb-3">
         <v-sheet width="calc(135rem - 200px)" color="#FFF" style="color: #114B5F">
-          <h3 class="pt-5 text-uppercase font-weight-light" style="padding-left: 5%;">{{ item.slogan }}</h3>
+          <h3 class="pt-5 text-uppercase font-weight-light" style="padding-left: 5%;">{{ categoria.nome_categoria }}</h3>
           <v-slide-group  class="pt-2 pb-4 pr-4 pl-4" show-arrows>
-              <v-slide-item v-for="imagem in livro" :key="imagem" v-show="imagem.categoria == livro.nomeLivro">
+              <v-slide-item v-for="livro,indexx in categoria.livros" :key="indexx">
                 <div class="ma-4" style="height: 100%; width: 8.1rem">
                 <v-row class="fill-height" justify="center">
-                  <img @click="irParaLivro(imagem)" id="img" class="ma-2" :src="imagem.imagemLivro" height="200" width="140" />
+                  <img @click="$router.push({name: 'livro', params: {id: livro.id}})" id="img" class="ma-2" :src="livro.capa_livro" height="200" width="140" />
                 </v-row>
               </div>
               </v-slide-item>
@@ -35,9 +29,10 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      props: [ "livro"],
+import axios from 'axios'
+export default {
+  data(){
+    return{
       model: 0,
       colors: [
         'primary',
@@ -46,36 +41,43 @@
         'red',
         'orange',
       ],
-      categorias: [
-        {title: "Ficção", slogan: "FICÇÕES PARA LER EM UM DIA", desc: "genero textual que tem como caracteristica..."},
-        {title: "Drama", slogan: "Dramas SUGERIDOS", desc: "genero textual que tem como caracteristica..."},
-        {title: "Suspense", slogan: "suspenses para curiosos", desc: "genero textual que tem como caracteristica..."},
-        {title: "Contos", slogan: "os melhores contos", desc: "genero textual que tem como caracteristica..."},
-      ],
-    }),
-    methods: {
-    irParaLivro(imagem) {
-      this.$router.push({
-        name: `livro`,
-        params: { livro: imagem, id: imagem.nomeLivro },
-      });
-    },
+      categorias: [],
+      imagens: [
+        require("../assets/categorias/image.png"),
+        require("../assets/categorias/image1.png"),
+        require("../assets/categorias/image2.png"),
+        require("../assets/categorias/image3.png"),
+      ]
+    }
   },
-  }
+  mounted(){
+    this.getCategorias()
+  },
+  methods: {
+    async getCategorias(){
+      const {data} = await axios.get('/api/categorias/')
+      this.categorias = data
+      console.log(this.categorias)
+    }
+  },
+}
 </script>
 
 <style>
-  /* STYLE DEFAULT */
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;700;800&display=swap');
-  
-  ::-webkit-scrollbar{
-    display: none;
-  }
-  * {
-      padding: 0;
-      margin: 0;
-      font-family: 'Inter', sans-serif;
-  }
-  /* ............. */
+/* STYLE DEFAULT */
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;700;800&display=swap');
 
+::-webkit-scrollbar{
+  display: none;
+}
+* {
+    padding: 0;
+    margin: 0;
+    font-family: 'Inter', sans-serif;
+}
+/* ............. */
+.imagem{
+  background-position: center;
+  background-size: cover;
+}
 </style>
