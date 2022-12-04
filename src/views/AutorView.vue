96a2bc48517694f689f873a5075212f23e8ee437
gabriel04alves <gabriel52.alves@gmail.com>
2022-12-04 00:43:11 -0300

    alterações na página do autor

--- src/views/AutorView.vue
+++ src/views/AutorView.vue
@@ -1,43 +1,49 @@
 <template>
-    <div class="d-flex justify-space-around align-center ma-16" style="gap: 5rem;">
-        <div class="" style="margin-left: 15rem;">
-        <div class="d-flex justify-center">
-            <v-avatar elevation="8" color="#667372" size="200"> <v-img :src="autor.autor_imagem"></v-img> </v-avatar>
+    <div >
+        <div>
+            <h1 class="d-flex align-self-center justify-center text-center text-uppercase font-weight-bold mt-10" style="color: #114B5F; "><v-icon class="mr-3" color="#114B5F" x-large>mdi-account-edit</v-icon> Autor</h1>
         </div>
-            <h3 class="text-center mt-4 font-weight-bold" style="color: #114B5F;">{{ autor.nome_autor }}</h3>
-            <p class="mt-4"> {{ autor.desc_autor }} </p>
-            <div class="d-flex justify-center align-center"><v-icon color="#114B5F">mdi-star-outline</v-icon> {{ autor.autor_nasc}}</div>
-            <div class="d-flex justify-center align-center"><v-icon color="#114B5F">mdi-coffin</v-icon> {{ autor.autor_falecimento ? autor.autor_falecimento : "Autor(a) vivo" }} </div>
-            <div class="d-flex justify-center align-center mt-1"><v-icon color="#114B5F">mdi-map-marker</v-icon> {{ autor.localAutor ? autor.localAutor : "Não cadastrado" }} </div>
-        </div>
-        <v-divider vertical></v-divider>
-        <div class="justify-center" style="">
-            <h3 class="text-center text-uppercase text-decoration-underline mb-0" style="color: #114B5F;">Obras mais populares</h3>
-            <div class="d-flex justify-center" >
-                <div class="d-flex justify-center flex-column ">
-                    <v-card class="rounded-lg mt-5" elevation="4" color="#114B5F" style="width: 45rem;" v-for="livro, index in livros.results" :key="index">
-                        <v-col class="d-flex align-center mt-2">
-                                <h4 class="font-weight-bold text-center ml-6" @click="$router.push({name: 'livro', params: {id: livro.id}})" style="color: #FFF; cursor: pointer">{{livro.titulo_livro}}</h4>
-                            <v-spacer></v-spacer>
-                            <v-rating class="" style="" background-color="#F1C40F" color="#F1C40F" empty-icon="mdi-star-outline" full-icon="mdi-star" half-icon="mdi-star-half-full" 
-                            hover length="5" readonly size="25" :value="livro.medianota"></v-rating> 
-                        </v-col>
-                        <v-col class="d-flex justify-space-around">
-                        <v-card class="rounded-lg" style="width: 25rem;">
-                                <p class="text-left ma-3"> {{livro.sinopse_livro | truncate(350)}} </p>
-                            </v-card>      
-                            <v-card class="rounded-lg" style="width: 12rem">
-                                <v-img @click="$router.push({name: 'livro', params: {id: livro.id}})" style="cursor: pointer" :src="livro.capa_livro" class="rounded-lg"> </v-img>
-                            </v-card>                        
-                        </v-col>
-                    </v-card>                        
-                </div>
+        <div class="d-flex justify-space-around align-start ma-16" style="gap: 5rem;">
+            <div class="" style="margin-left: 15rem;">
+            <div class="d-flex justify-center">
+                <v-avatar elevation="8" color="#667372" size="200"> <v-img :src="autor.autor_imagem"></v-img> </v-avatar>
             </div>
-                <div class="d-flex justify-center mt-6" style="gap: 10px">
-                    <v-btn color="#caf1ff" v-if="livros.previous" @click="page--">Anterior</v-btn>
-                    <v-card v-if="livros.count > 2" width="30px" class="d-flex align-center justify-center" style="background-color: #114b5f; color: white; font-size: 20px;">{{page}}</v-card>
-                    <v-btn color="#caf1ff" v-if="livros.next" @click="page++">Próximo</v-btn>
+                <h3 class="text-center mt-4 font-weight-bold" style="color: #114B5F;">{{ autor.nome_autor }}</h3>
+                <p class="mt-4"> {{ autor.desc_autor }} </p>
+                <div class="d-flex justify-center align-center"><v-icon color="#114B5F">mdi-star-outline</v-icon> {{ autor.autor_nasc}}</div>
+                <div class="d-flex justify-center align-center"><v-icon color="#114B5F">mdi-coffin</v-icon> {{ autor.autor_falecimento ? autor.autor_falecimento : "Autor(a) vivo" }} </div>
+            </div>
+
+            <v-divider class="ma-15" color="#114B5F" vertical></v-divider>
+            
+            <div class="justify-center" style="">
+                <h2 class="text-center text-uppercase text-decoration-underline mb-0" style="color: #114B5F;">Obras mais populares</h2>
+                <div class="d-flex justify-center" >
+                    <div class="d-flex justify-center flex-column ">
+                        <v-card class="rounded-lg mt-5" elevation="4" color="#114B5F" style="width: 45rem;" v-for="livro, index in livros.results" :key="index">
+                            <v-col class="d-flex align-center mt-2">
+                                    <h3 class="font-weight-bold text-center ml-6" @click="$router.push({name: 'livro', params: {id: livro.id}})" style="color: #FFF; cursor: pointer">{{livro.titulo_livro}}</h3>
+                                <v-spacer></v-spacer>
+                                <v-rating class="" style="" background-color="#F1C40F" color="#F1C40F" empty-icon="mdi-star-outline" full-icon="mdi-star" half-icon="mdi-star-half-full" 
+                                hover length="5" readonly size="25" :value="livro.medianota"></v-rating> 
+                            </v-col>
+                            <v-col class="d-flex justify-space-around">
+                            <v-card class="rounded-lg" style="width: 25rem;">
+                                    <p class="text-left ma-3"> {{livro.sinopse_livro | truncate(350)}} </p>
+                                </v-card>      
+                                <v-card class="rounded-lg" style="width: 12rem">
+                                    <v-img @click="$router.push({name: 'livro', params: {id: livro.id}})" style="cursor: pointer" :src="livro.capa_livro" class="rounded-lg"> </v-img>
+                                </v-card>                        
+                            </v-col>
+                        </v-card>                        
+                    </div>
                 </div>
+                    <div class="d-flex justify-center mt-6" style="gap: 10px">
+                        <v-btn color="#caf1ff" v-if="livros.previous" @click="page--"> <v-icon color="#114B5F">mdi-chevron-left</v-icon> </v-btn>
+                        <v-card v-if="livros.count > 2" width="30px" class="d-flex align-center justify-center" style="background-color: #114b5f; color: white; font-size: 20px;">{{page}}</v-card>
+                        <v-btn color="#caf1ff" v-if="livros.next" @click="page++"> <v-icon color="#114B5F">mdi-chevron-right</v-icon> </v-btn>
+                    </div>
+            </div>
         </div>
     </div>
 </template>
@@ -77,5 +83,9 @@ export default {
 </script>
 
 <style>
-
+* {
+  padding: 0;
+  margin: 0;
+  font-family: "Inter", sans-serif;
+}
 </style>
